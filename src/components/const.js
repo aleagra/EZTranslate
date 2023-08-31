9a969bc025ac265b6c8bad33c7ebfc33ee926273
Alejandro Agra <aleagra08@hotmail.com>
2023-08-31 14:12:49 -0300

    .

---
+++ src/components/const.js
@@ -0,0 +1,8 @@
+const config = {
+    REACT_APP_KEY:"5a42cacf00msh6b85df453f74aaep10ad5ajsn6ef9bddc4f5c",
+    REACT_APP_HOST:"microsoft-translator-text.p.rapidapi.com",
+    REACT_APP_DETECTOR:"https://microsoft-translator-text.p.rapidapi.com/Detect",
+    REACT_APP_TRANSLATE:"https://microsoft-translator-text.p.rapidapi.com/translate"
+  };
+  
+  export default config;
